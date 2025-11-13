6ac1f4068ab4d4354c6a78f86d4db82dc2f96539
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-13 16:16:56 -0300

    feat

---
+++ logica/nomes.js
@@ -0,0 +1,4 @@
+const names = ["-B2", "-Rodrigo", "-Victoria", "-Dudu"];
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
---
+++ logica/numeros-for.js
@@ -0,0 +1,10 @@
+const numbers = [0,1,2,3,4,5];
+for (let i = 0; i < numbers.length; i++) {
+  console.log(`- Esse numero é ${i}`);
+}
+
+console.log("---------------------------------")
+
+for (let i = 0; i <= 10; i++){
+    console.log(`Esta é a ${i}ª vez que esta frase é exibida na tela.`)
+}
