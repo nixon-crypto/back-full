c99f005ebcc4087d33ccb5aa58bf3d0d3e877c50
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-13 15:34:42 -0300

    feat

--- logica/pontos.js
+++ logica/pontos.js
@@ -1,5 +1,15 @@
-// 
+//
 const vitorias = Number(prompt("Quantos jogos seu time venceu?"));
 const empates = Number(prompt("Quantos jogos seu time empatou?"));
 
-const pontos = (vitorias * 3) + empates;
+const pontos = vitorias * 3 + empates;
+
+function mostra(mensagem) {
+  document.write(mensagem + pontos, " pontos");
+}
+if (pontos > 10) {
+  mostra("Classificado");
+} else if (pontos < 5){
+  mostra("Seu time precisa melhorar");
+}
+// mostra();
