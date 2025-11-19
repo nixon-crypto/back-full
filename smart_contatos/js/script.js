1644fa1b17aa176f32d56d05f7a2940e56e1c3e1
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-19 14:46:16 -0300

    ocultando-lista

---
+++ smart_contatos/js/script.js
@@ -0,0 +1,46 @@
+const form = document.querySelector("form");
+const lista = document.querySelector(".lista");
+const inputNome = document.getElementById("nome");
+const inputEmail = document.getElementById("email");
+const inputTel = document.getElementById("telefone");
+const listaMsg = document.querySelector(".lista-msg");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Validação do formulário
+  if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
+    alert("Digite seus dados");
+    return false;
+  }
+
+  // condição para retirar a li > .lista-msg
+
+  // Minha logica
+  //  if (inputNome.value != "" || inputEmail.value != "" || inputTel.value != ""){
+
+  // document.querySelector(".lista-msg").style.display = "none"
+  // listaMsg.style.display="none"
+  
+  
+  if (listaMsg) {
+    listaMsg.remove(); // Modo simplificado
+  }
+
+  // Criar LI
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+        <span class="contato-nome">${inputNome.value}</span>
+        <span class="contato-email">${inputEmail.value}</span>
+        <span class="contato-telefone">${inputTel.value}</span>
+    `;
+
+  console.log(li);
+
+  // appendChild()
+  lista.appendChild(li);
+
+  // Limpar inputs
+  form.reset();
+});
