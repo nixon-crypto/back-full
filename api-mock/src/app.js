df2a42694783586e73445e1ec6a293d8f1676ba7
Stefany-viveiros <viveirosstefany@gmail.com>
2025-12-02 15:45:05 -0300

    feat:post e put

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Olá mundo!");
@@ -37,16 +38,53 @@ const cadastros = [
   },
 ];
 
-// function buscarNomeId(){
-//     cadastros = (req.params.id)
-// }
+function buscarNomeId(id) {
+ return cadastros.find((nome) => nome.id == id);
+}
 
-app.get("/cadastro", (req,res) =>{
+function buscarIdNome(id){
+  return cadastros.findIndex((nome) => nome?.id == id);
+}
+
+app.get("/cadastro", (req, res) => {
     res.send(cadastros);
 });
 
-app.post("/cadastro:id", (req,res) =>{
-    res.send(cadastros);
+app.get("/cadastro:id", (req, res) => {
+  const pessoa = buscarNomeId(req.params.id);
+  if (!pessoa) {
+    return res.status(404).send("Nome não encontrado!");
+  }
+  res.json(pessoa);
 });
 
-// app.put("")
+// cadastrar pessoas por id
+app.post("/cadastro", (req, res) => {
+  cadastros.push(req.body.id);
+  res.status(201).send("Nome cadastrado com sucesso!");
+
+  res.json(cadastros);
+});
+
+app.put("/cadastro:id", (req, res) => {
+  let index = buscarIdNome(req.params.id);
+  if (index === -1) return res.status(404).send(`Nenhum nome com o id ${req.params.id} foi encontrado!`);
+
+  if(!req.body.nome || !req.body.idade){
+    return res.status(400).send(`Nome e idade são obrigatórios!`);
+  }
+  cadastros[index].nome = req.body.nome;
+  cadastros[index].idade = req.body.idade;
+
+  res.json(cadastros);
+});
+
+app.delete("/cadastro:id", (req, res) =>{
+  let id = req.params.id;
+  let index = buscarIdNome(id);
+  if(index === -1){
+    return res.status(404).send(`Nenhum nome com id ${id} foi encontrado.`);
+  }
+  cadastros.splice(index, 1);
+  res.send(`Nome com id ${req.params.id} excluida com sucesso !`);
+});
